a2142f138bc30045b9483b7545da632f93cc2a44
iamgabrielsoft <iamgabrielsoft@gmail.com>
2021-11-28 01:13:07 +0100

    new

---
+++ src/constants.ts
@@ -0,0 +1,84 @@
+import { AppProps, } from "./@types/types";
+import {  FontFamilyValues } from './@types/element'
+
+export const FONT_FAMILY = {
+    Virgil: 1,
+    Helvetica: 2,
+    Cascadia: 3,
+  };
+
+
+  export const MODES = {
+    VIEW: "viewMode",
+    ZEN: "zenMode",
+    GRID: "gridMode",
+  };
+
+
+
+export const DEFAULT_UI_OPTIONS: AppProps["UIOptions"] = {
+    canvasActions: {
+      changeViewBackgroundColor: true,
+      clearCanvas: true,
+      export: { saveFileToDisk: true },
+      loadScene: true,
+      saveToActiveFile: true,
+      theme: true,
+      saveAsImage: true,
+    },
+}
+
+export const EXPORT_SCALES = [1, 2, 3];
+export const DEFAULT_FONT_SIZE = 20;
+export const DEFAULT_FONT_FAMILY: FontFamilyValues = FONT_FAMILY.Virgil;
+export const DEFAULT_TEXT_ALIGN = "left";
+export const DEFAULT_VERTICAL_ALIGN = "top";
+export const DEFAULT_VERSION = "{version}";
+
+
+
+export enum EVENTS {
+  COPY = "copy",
+  PASTE = "paste",
+  CUT = "cut",
+  KEYDOWN = "keydown",
+  KEYUP = "keyup",
+  MOUSE_MOVE = "mousemove",
+  RESIZE = "resize",
+  UNLOAD = "unload",
+  BLUR = "blur",
+  DRAG_OVER = "dragover",
+  DROP = "drop",
+  GESTURE_END = "gestureend",
+  BEFORE_UNLOAD = "beforeunload",
+  GESTURE_START = "gesturestart",
+  GESTURE_CHANGE = "gesturechange",
+  POINTER_MOVE = "pointermove",
+  POINTER_UP = "pointerup",
+  STATE_CHANGE = "statechange",
+  WHEEL = "wheel",
+  TOUCH_START = "touchstart",
+  TOUCH_END = "touchend",
+  HASHCHANGE = "hashchange",
+  VISIBILITY_CHANGE = "visibilitychange",
+  SCROLL = "scroll",
+}
+
+export const TOAST_TIMEOUT = 5000;
+export const CURSOR_TYPE = {
+  TEXT: "text",
+  CROSSHAIR: "crosshair",
+  GRABBING: "grabbing",
+  GRAB: "grab",
+  POINTER: "pointer",
+  MOVE: "move",
+  AUTO: "",
+};
+
+
+export const POINTER_BUTTON = {
+  MAIN: 0,
+  WHEEL: 1,
+  SECONDARY: 2,
+  TOUCH: -1,
+};
