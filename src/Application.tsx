b5b7f49f3c6a9341d37b2261384473983903423d
iamgabrielsoft <iamgabrielsoft@gmail.com>
2021-11-29 11:22:33 +0100

    app file

---
+++ src/Application.tsx
@@ -0,0 +1,13 @@
+import React, { FC, useEffect } from 'react';
+import Navigation from './Navigation/Route'
+
+
+const App:FC<{}> = () => {
+  return (
+    <Navigation/>
+  )
+}
+
+
+
+export default App;
